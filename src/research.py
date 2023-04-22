ead2394c52a93648cbbfcf02d8305f4da0e577f4
BelkanWar <challenger1501@gmail.com>
2023-04-22 22:58:22 +0800

    2023-04-22 progress

--- src/research.py
+++ src/research.py
@@ -4,10 +4,12 @@ os.chdir(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 sys.path.append("src/")
 import utils
 import numpy as np
+from geopy import distance
 from multiprocessing import Pool
 from sklearn.decomposition import TruncatedSVD
 
 data = utils.read_dmp_data("data/dmp_loc_traces_Feb10to28_sample100IDs.csv")
+fix_map = False
 
 def pipeline(uuid):
     print(uuid)
@@ -38,10 +40,27 @@ def pipeline(uuid):
 
     # each cluster represents a potential routing track
     result = np.dot(PC_weight_mean_array, H)
-    painter.plot_map(matrix, f"{uuid}_footprint", fix_map=False)
-    painter.plot_map(result, f"{uuid}_PC1", fix_map=False)
+    painter.plot_gif(matrix, f"{uuid}_footprint", fix_map=fix_map)
+    painter.plot_gif(result, f"{uuid}_PC1", fix_map=fix_map)
+
+    # figure out where home/office is
+    '''
+    latlon_list = []
+    cluster_input = []
+    for row in range(result.shape[0]):
+        for col in range(1, 96):
+            previous_lat, previous_lon = result[row, col-1], result[row, col+95]
+            lat, lon = result[row, col], result[row, col+96]
+            latlon_list.append([lat, lon])
+            cluster_input.append([lat, lon, distance.distance((lat, lon), (previous_lat, previous_lon)).m])
 
 
+    cluster = OPTICS().fit(np.array(cluster_input))
+    group = cluster.labels_
+    painter.plot_map(latlon_list, group, f"{uuid}_display", fix_map=fix_map)
+    '''
+    
 with Pool() as pool:
     pool.map(pipeline, list(data))
+
 
